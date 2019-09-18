37cf1ffe0bd76848d03fcff5cbbe011a0c743c7e
Shinnosuke Sawada <6warashi9@gmail.com>
2019-09-18 09:52:56 +0900

    minifyとかしないことにした

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ PUBLISH_DIR="public"
 git config --global user.email "action@github.com"
 git config --global user.name "GitHub Action"
 
-HUGO_ENV=production hugo -v --gc --minify --cleanDestinationDir
+HUGO_ENV=production hugo -v
 
 TMPDIR="$(mktemp -d)"
 git clone --depth 1 -b "${PUBLISH_BRANCH}" "https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" "${TMPDIR}"
